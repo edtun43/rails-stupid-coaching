b7850e8416f24a99864c827578455a0f73c0d4e9
Edin Tuna <ed.tun@outlook.com>
2023-02-20 12:51:50 +0100

    gg

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = if params[:question] == 'I am going to work'
+                'Great!'
+              elsif params[:question].include? '?'
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+
+    @question = params[:question]
+  end
+end
